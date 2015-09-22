6a335cee5dacf5a56b04b499374e5a8d69469294
AbelSu <abel@abeldemacbook-pro.local>
2015-09-22 22:22:13 +0800

    Initial Commit

---
+++ NSThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSThread
+//
+//  Created by abel on 15/9/22.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
